0ceeaa99a6a84f8848c47158042393d178d06100
growthvortexagency <growthvortexagency@gmail.com>
2024-12-03 19:30:47 +0530

    Updated on 3rd December 2024

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -203,3 +203,48 @@ document.getElementById('naac').addEventListener('click', function () {
 document.getElementById('faculties').addEventListener('click', function () {
 	window.location.href = 'faculties.html';
 });
+
+
+
+
+
+
+
+var app = {
+	settings: {
+	  container: $('.calendar'),
+	  calendar: $('.front-1'),
+	  days: $('.weeks span'),
+	  form: $('.back'),
+	  input: $('.back input'),
+	  buttons: $('.back button')
+	},
+  
+	init: function() {
+	  instance = this;
+	  settings = this.settings;
+	  this.bindUIActions();
+	},
+  
+	swap: function(currentSide, desiredSide) {
+	  settings.container.toggleClass('flip');
+  
+	  currentSide.fadeOut(900);
+	  currentSide.hide();
+	  desiredSide.show();
+  
+	},
+  
+	bindUIActions: function() {
+	  settings.days.on('click', function(){
+		instance.swap(settings.calendar, settings.form);
+		settings.input.focus();
+	  });
+  
+	  settings.buttons.on('click', function(){
+		instance.swap(settings.form, settings.calendar);
+	  });
+	}
+  }
+  
+  app.init();
