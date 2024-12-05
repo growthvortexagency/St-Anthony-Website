2c4527cbb8dfdfaf48703459e4f59bc5ad143aa4
growthvortexagency <growthvortexagency@gmail.com>
2024-12-05 18:49:28 +0530

    Updated on 5th December 2024 at 6:48 PM

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -248,3 +248,18 @@ var app = {
   }
   
   app.init();
+
+
+
+  $(document).ready(function() {
+
+	$(`[unique-script-id="w-w-dm-id"] .btn-box`).click(function() {
+	  $(this).parent().children(".overlay").show();
+  
+	});
+  
+  
+	$(`[unique-script-id="w-w-dm-id"] .close`).click(function() {
+	  $(".overlay").hide();
+	});
+  });
